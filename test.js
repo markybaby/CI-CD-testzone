2b846648ce23ccb2522082b2df2211a6cf3d7bf7
markybaby <markgd0210@gmail.com>
2024-08-30 18:41:52 +0800

    messages for ben testing

--- test.js
+++ test.js
@@ -5,6 +5,11 @@ app.get('/hello', (req, res) => {
 res.send('<h1> Hello, World! </h1>');
 });
 
+app.get('/ben', (req, res) => {
+    res.send('<h1> Greeting, Ben </h1> <br> <h2> How are you doing today? </h2> <br> <h3> I hope you are having a great day! </h3> <br> <h4> Rajesh my Balans uWu </h4>'); 
+});
+
 app.listen(8000, () => {
 console.log(`Server is listening at http://localhost:8000`);
 });
+
